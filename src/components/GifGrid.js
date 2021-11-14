2f9aecb5f1e72757ff3b069b75d596e7ee9630d9
oscardenas <oscardenas17@gmail.com>
2021-11-14 15:08:57 -0500

    useFetchGift- Hook - obtener imagenes y loading

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,45 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( {category}) => {
   
-    
-const [images, setImages] = useState([]);
-    
-    // one time ejec
-    useEffect( ()=>{
-        getGifs(category)
-                 .then(setImages)
-    }, [category]);
-
-  
-  
+    // const state = useFetchGifs(); // hook
+    const {data: images, loading} = useFetchGifs(category);
 
     return (
         <>
-        <h3>{category}</h3>
-
-        <div className="card-grid">
-               
-            {
-                       images.map( img => (
-                           <GifGridItem  
+            <h3>{category}</h3>
+         
+            {loading && <p>Loading</p>}
+            <div className="card-grid">
+                
+                {
+                    images.map( img => (
+                        <GifGridItem  
                                 key={img.id}
                                 // img= {img} />
                                 {...img} 
                             />
-                       ))
-            }
+                    ))
+                }
+                
             
-                   {/* {
-                       images.map( {id, title} > (
-                           <li key={id}> {title} </li>
-                        ) )
-                   } */}
-            
-        
-        </div></>
+            </div>            
+         </> 
+
     )
 }
+
  
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import { getGifs } from "../helpers/getGifs";
+
+
+export const useFetchGifs = ( category ) =>{
+
+
+    const [state, setstate] = useState({
+    data: [],
+    loading: true
+        // one time ejec
+  
+
+    });
+          
+    useEffect( ()=>{
+        getGifs(category)
+                 .then( imgs => {
+
+                    setTimeout( () =>{
+                        
+                        setstate({
+                            data: imgs,
+                            loading: false
+                        });
+
+                    },3000);
+
+                 })
+    }, [category]);
+
+
+    return state; //{ data: [], loading: true}
+}
