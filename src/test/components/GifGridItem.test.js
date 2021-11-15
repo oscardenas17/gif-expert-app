8913bbcab581177989a3710a8b57d5e7e49dc3f1
oscardenas <oscardenas17@gmail.com>
2021-11-15 12:21:05 -0500

    Pruebas en componente - gifGridItem

--- src/test/components/GifGridItem.test.js
+++ src/test/components/GifGridItem.test.js
@@ -6,7 +6,7 @@ describe('Pruebas en <GifGridItem />', () => {
     
     const title = 'Un titulo';
     const url = 'https//locahost/imagen.jpg';
-
+    const wrapper = shallow (<GifGridItem  title={title} url = {url}/>)
 
     test('Debe mostar el componente correctamente', () => {
         
@@ -14,6 +14,32 @@ describe('Pruebas en <GifGridItem />', () => {
         expect(wrapper).toMatchSnapshot();
 
     })
-    
+
+    test('Debe tener un párrafo con el title', () => {
+        
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    })
+
+    test('La imagen tiene un title y una url', () => {
+        
+        const img = wrapper.find('img');
+        // console.log(img.prop('src'));
+        expect( img.prop('src') ).toBe(url);
+        expect( img.prop('alt') ).toBe(title);
+    })
+
+
+    test('contiene clase animate_', () => {
+        
+        const div = wrapper.find('div');
+        // console.log(img.prop('src'));
+        const className = div.prop('className')
+
+        expect( className.includes('animate_fadeIn') ).toBe( true);
+      
+    })
+
+
 
 })
