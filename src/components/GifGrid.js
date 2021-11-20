46cdada39e7d99131c5e5d0ced45d8c8fad8f34a
oscardenas <oscardenas17@gmail.com>
2021-11-19 21:26:57 -0500

    Pruebas GifGrid- mock customHook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+
 import { useFetchGifs } from '../hooks/useFetchGifs'
 import { GifGridItem } from './GifGridItem';
 
@@ -29,6 +31,10 @@ export const GifGrid = ( {category}) => {
          </> 
 
     )
+}
+
+GifGrid.propTypes  = {
+    category: PropTypes.string.isRequired
 }
 
  
