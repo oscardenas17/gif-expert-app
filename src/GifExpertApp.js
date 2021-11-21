9f09caa497f7768f62846eb84c711d17dfe2711c
oscardenas <oscardenas17@gmail.com>
2021-11-21 09:20:45 -0500

    Pruebas del componente GifExpertApp

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,11 +3,13 @@ import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
 
-export const GifExpertApp = () => {
+export const GifExpertApp = ( {defaultCategories = []}) => {
 
     // const categories = ['One Punch','Samurai X','Dragon Ball'];
     //Hook de useState - 
-    const [categories, setCategories] = useState(['One Punch']);
+    // const [categories, setCategories] = useState(['One Punch']);
+    const [categories, setCategories] = useState(defaultCategories);
+
 
     // const handleAdd = () =>{
     //     setcategories( [...categories, 'HunterXhunter'] );
